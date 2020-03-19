1b21d09889ff49dd3800ff517c5a13e5d86c99c0
olivermicke <olivermicke@users.noreply.github.com>
2020-03-19 22:22:04 +0000

    deploy: 5897803fc0a9fd6dc68f3b3bb9678979901f7c39

---
+++ precache-manifest.d0fe4f66839d63f5e6e30c596c7860b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aa630c91233b792742ba709432a53a2",
+    "url": "/monthly-budget-planner/index.html"
+  },
+  {
+    "revision": "19195eef720af398216b",
+    "url": "/monthly-budget-planner/static/css/main.41726d72.chunk.css"
+  },
+  {
+    "revision": "65aa24ebfb60335b2b94",
+    "url": "/monthly-budget-planner/static/js/2.3a4a4ed6.chunk.js"
+  },
+  {
+    "revision": "4fa956aefaaf894a4cc4dacfd7b57d8c",
+    "url": "/monthly-budget-planner/static/js/2.3a4a4ed6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19195eef720af398216b",
+    "url": "/monthly-budget-planner/static/js/main.949b53e1.chunk.js"
+  },
+  {
+    "revision": "d06c77fefb6d71b55e01",
+    "url": "/monthly-budget-planner/static/js/runtime-main.c7773bec.js"
+  }
+]);
